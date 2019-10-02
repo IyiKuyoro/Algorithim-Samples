b45e28ad04c562d6485d3d8a7065a9539bc44076
IyiKuyoro <opeoluwa.iyikuyoro@gmail.com>
2019-10-02 22:49:29 +0100

    singly linked list

---
+++ DataStructures/SinglyLinkedList.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+}
+
+const list = new SinglyLinkedList();
+list.push(1);
+list.push(2);
